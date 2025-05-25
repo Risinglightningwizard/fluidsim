4f745aef3be1db637dd406178bbe101ade157c2c
GerritBeam <risinglightningwizard@gmail.com>
2025-05-25 16:14:18 -0700

    Initial upload
    No fluid sim yet. Just some initial set up for compute shaders and testing.

---
+++ imageeditcompute/imageedit.gd
@@ -0,0 +1,56 @@
+extends Sprite2D
+var output_texture_width := 10
+var output_texture_height := 10
+@export var curve = Curve
+func _ready():
+	# Create a local rendering device.
+	var rd := RenderingServer.create_local_rendering_device()
+	# Load GLSL shader
+	var shader_file := load("res://imageedit.glsl")
+	var shader_spirv: RDShaderSPIRV = shader_file.get_spirv()
+	var shader := rd.shader_create_from_spirv(shader_spirv)
+	
+	# create texture format
+	var fmt := RDTextureFormat.new()
+	fmt.width = output_texture_width
+	fmt.height = output_texture_height
+	fmt.format = RenderingDevice.DATA_FORMAT_R32G32B32A32_SFLOAT
+	fmt.usage_bits = RenderingDevice.TEXTURE_USAGE_CAN_UPDATE_BIT | \
+		RenderingDevice.TEXTURE_USAGE_STORAGE_BIT | RenderingDevice.TEXTURE_USAGE_CAN_COPY_FROM_BIT
+	
+	#create image
+	var view := RDTextureView.new()
+	var output_image := Image.create(output_texture_width, output_texture_height, false, Image.FORMAT_RGBAF)
+	var output_tex = rd.texture_create(fmt, view, [output_image.get_data()])
+	
+	#create uniform and uniform set
+	var output_tex_uniform := RDUniform.new()
+	output_tex_uniform.uniform_type = RenderingDevice.UNIFORM_TYPE_IMAGE
+	output_tex_uniform.binding = 0
+	output_tex_uniform.add_id(output_tex)
+
+	var uniform_set:= rd.uniform_set_create([output_tex_uniform], shader, 0) 
+	
+	# Create a compute pipeline
+	var pipeline := rd.compute_pipeline_create(shader)
+	var compute_list := rd.compute_list_begin()
+	rd.compute_list_bind_compute_pipeline(compute_list, pipeline)
+	rd.compute_list_bind_uniform_set(compute_list, uniform_set, 0)
+	rd.compute_list_dispatch(compute_list, 1, 1, 1)
+	rd.compute_list_end()
+	
+	# Submit to GPU and wait for sync
+	rd.submit()
+	rd.sync()
+	
+	#retrieve data
+	var output_bytes := rd.texture_get_data(output_tex,0)
+	var output := output_bytes.to_float32_array()
+	print("Output: ", output)
+	
+	var finalImage := Image.create_from_data(output_texture_width, output_texture_height,\
+		 false, Image.FORMAT_RGBAF, output_bytes)
+	
+	var finaltex := ImageTexture.create_from_image(finalImage)
+	#self.texture = finaltex
+	material.set("shader_parameter/fluid", finaltex)
---
+++ imageeditcompute/imageedit.glsl
@@ -0,0 +1,15 @@
+#[compute]
+#version 450
+
+//float computemax = GL_MAX_COMPUTE_WORK_GROUP_SIZE;
+
+layout(local_size_x = 10, local_size_y = 10, local_size_z = 1) in;
+
+layout(set = 0, binding = 0, rgba32f) uniform image2D OUTPUT_TEXTURE;
+
+void main() {
+    ivec2 texel = ivec2(gl_GlobalInvocationID.xy);
+    ivec2 dimensions = imageSize(OUTPUT_TEXTURE);
+    vec4 color = vec4(float(texel.x)/dimensions.x,float(texel.y)/dimensions.y,-3,1);
+    imageStore(OUTPUT_TEXTURE, texel, color);
+}
